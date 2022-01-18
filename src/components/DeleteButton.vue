d5407c1307422693e89143e7eb221c61d98c1b84
maryanna <prelecnaja@gmail.com>
2022-01-19 00:48:18 +0200

    weert

---
+++ src/components/DeleteButton.vue
@@ -0,0 +1,40 @@
+<template>
+    <span class="material-icons icon_delete" @click="removeItem">
+        delete
+    </span>
+</template>
+
+<script>
+export default {
+    props: [
+        'element',
+    ],
+    data(){
+        return{
+            isLoading: false,
+        }
+    },
+    methods:{
+        async removeItem(){
+            
+            this.isLoading = true;
+            let response = await fetch(`https://61db46774593510017aff868.mockapi.io/api/v1/item/${this.element.id}`,{
+            method: 'DELETE',
+            });
+            let json = await response.json();
+            console.log(json);
+            this.$emit('deleted');
+            this.isLoading = false;
+        },
+    }
+}
+
+</script>
+
+<style>
+.icon_delete {
+    background: #F57474;
+    border-radius: 50px;
+    cursor: pointer;
+  }
+</style>
